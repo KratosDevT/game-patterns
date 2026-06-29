992bd9aa3f651aebdde5ab4d450f2cf6857c74b2
KratosDevT <simone.trevisan.gamedev@gmail.com>
2026-06-29 23:28:50 +0200

    first commit fsm states

---
+++ command-pattern/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+//command virtuale
+class Command
+{
+public:
+    virtual ~Command() = default;
+    virtual void execute() = 0;
+};
+
+// stato fsm astratto
+class FSMState
+{
+
+public:
+    virtual ~FSMState() = default;
+
+    virtual const char* name() const = 0;
+    virtual void enter() = 0;
+    virtual void exit() = 0;
+};
+
+class FSMTransition
+{
+public:
+    const char* name;
+    FSMState* stateFrom;
+    FSMState* stateTo;
+
+};
+
+// classe astratta di FSM
+class FSM
+{
+
+public:
+    virtual ~FSM() = default;
+    void addState(FSMState* state)
+    {
+        states.emplace(state->name(), state);
+    };
+
+    void addTransition(){};
+
+private:
+    FSMState* CurrentState;
+    std::unordered_map<std::string, FSMState*> states;
+    std::unordered_map<FSMState*, std::vector<FSMTransition>> transitions;
+};
+
+// implementazioni
+
+class MenuFSM : public FSM
+{
+
+};
+
+class FSMTransitionCommand : public Command
+{
+    virtual void execute()
+    {
+
+    }
+};
+
+int main()
+{
+    std::cout << "C++ standard: " << __cplusplus << "\n";
+
+
+
+    
+    return 0;
+};
